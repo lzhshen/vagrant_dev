b9ebe29b1480db46caf862142237b957e5b7b2f8
lzhshen <shen@localhost.localdomain>
2018-05-21 02:56:30 -0400

    add bootstrap script

--- Vagrantfile
+++ Vagrantfile
@@ -6,5 +6,7 @@
 # backwards compatibility). Please don't change it unless you know what
 # you're doing.
 Vagrant.configure("2") do |config|
+  config.vm.synced_folder ".", "/vagrant_data"
   config.vm.box = "centos/7"
+  config.vm.provision :shell, path: "./bootstrap.sh"
 end
---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+sudo yum -y update
+sudo yum -y install yum-utils
+sudo yum -y groupinstall development
+sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+sudo yum -y install python36u
+sudo yum -y install python36u-pip
+sudo yum -y install python36u-devel
+sudo yum install -y epel-release
+sudo yum -y install ansible
+
+sudo pip3.6 install virtualenv
